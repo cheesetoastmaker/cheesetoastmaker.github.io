d0ca53d77ccce66bab607b0ac3214baab6ba9ab4
cheesetoastmaker <111494358+cheesetoastmaker@users.noreply.github.com>
2022-08-26 21:24:09 +0530

    Delete AugustQuiz/Aug2 directory

--- AugustQuiz/Aug2/script.js
+++
@@ -1,7 +0,0 @@
-// Responsive table method using display:block and data attributes
-// Thanks to @leefroese for suggestion about data attributes
-document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
-document.getElementsByTagName("p")[0].style.fontSize = "6vw";
-document.getElementsByTagName("h2")[0].style.fontSize = "6vw";
-document.getElementsByTagName("ul")[0].style.fontSize = "6vw";
-document.getElementsByTagName("tr")[0].style.fontSize = "6vw";

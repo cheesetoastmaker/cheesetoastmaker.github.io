8bfd11a0c54f3c67e1e779cd3c7c16ef50583fd2
cheesetoastmaker <111494358+cheesetoastmaker@users.noreply.github.com>
2022-08-26 21:22:06 +0530

    Update script.js

--- AugustQuiz/Aug2/script.js
+++ AugustQuiz/Aug2/script.js
@@ -1,2 +1,6 @@
 // Responsive table method using display:block and data attributes
 // Thanks to @leefroese for suggestion about data attributes
+document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+document.getElementsByTagName("p")[0].style.fontSize = "6vw";
+document.getElementsByTagName("h2")[0].style.fontSize = "6vw";
+document.getElementsByTagName("ul")[0].style.fontSize = "6vw";
